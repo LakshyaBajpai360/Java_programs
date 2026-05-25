3d1bea14d4c1317433cd7cc6342a0574f188d84c
Lakshyabajpai360 <lakshbajpai373@gmail.com>
2026-05-25 22:26:10 +0530

    initial commit

---
+++ src/Pr1.java
@@ -0,0 +1,21 @@
+public class Pr1 {
+    public static void main(String[] args) {
+        int roll = 20;
+        String S="LAKKU";
+        float per=96.56f;
+        char grade='a';
+        double high=98.555555;
+
+        System.out.println("-----Student details-----");
+        System.out.println("student name is "+S);
+
+        System.out.println("Grade "+grade);
+        System.out.println("Roll no of "+S+" is "+roll);
+        System.out.println("Percentage of " +S+ " is " +per);
+        System.out.println("highest in class" +high);
+
+
+
+
+    }
+}
