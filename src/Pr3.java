6628460b77cccaef0c535498c7829d06da488aeb
Lakshyabajpai360 <lakshbajpai373@gmail.com>
2026-05-26 17:19:08 +0530

    fourth commit

---
+++ src/Pr3.java
@@ -0,0 +1,16 @@
+//simple addition
+import java.util.Scanner;
+public class Pr3 {
+    public static void main(String[] args) {
+        double a,b,c;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("ENTER FIRST VALUE: ");
+        a=sc.nextDouble();
+        System.out.println("ENTER THE SECOND VALUE: ");
+        b= sc.nextDouble();
+        c=a+b;
+        System.out.println("THE RESULT IS: "+c);
+
+
+    }
+}
