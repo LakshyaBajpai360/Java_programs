c50ec8efd2f2614b925491def4f64105262c708a
Lakshyabajpai360 <lakshbajpai373@gmail.com>
2026-05-29 18:08:58 +0530

    seventh commit

---
+++ src/Pr10.java
@@ -0,0 +1,14 @@
+//Do while loop
+
+public class Pr10 {
+    public static void main(String[] args) {
+        int i=1;
+        do {
+            System.out.println("john the don");
+
+            i++;
+        }
+        while (i<=5);
+
+    }
+}
---
+++ src/Pr8.java
@@ -0,0 +1,10 @@
+//for loop
+public class Pr8 {
+    public static void main(String[] args) {
+        int i;
+        for (i=1;i<=5;i++){
+
+            System.out.println("john the don ");
+        }
+    }
+}
